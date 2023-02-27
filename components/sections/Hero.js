c310f663f41b4170352b362fd2308828e83bb92e
MugaboDenys <mugabodan2020@gmail.com>
2023-02-27 19:11:22 +0200

    working on menu

--- components/sections/Hero.js
+++ components/sections/Hero.js
@@ -10,7 +10,7 @@ const Hero = () => {
         
         <div className="relative">
             
-            <div className="bg-black text-white  md:absolute md:left-28 md:-bottom-10 md:w-[40rem] py-7 md:px-10 px-5">
+            <div className="bg-black text-white  md:absolute md:left-28 md:-bottom-20 md:w-[40rem] z-20 py-7 md:px-10 px-5">
                 <h3 className="text-3xl">How tech is transforming diagnostics and care</h3>
                 <p>Read three inspiring stories of how Aaltonians are bringing new hope to people who suffer from</p>
                 <Link href={'/'} className="flex w-fit items-center gap-2 hover:bg-white hover:text-black duration-200" > <BsArrowRight/> How it works</Link>
